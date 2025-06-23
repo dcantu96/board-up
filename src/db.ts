d24544c33561967b0b1de5cc43fc40d0a589d545
David Cantu <davidcantu966@gmail.com>
2025-06-23 12:52:05 -0600

    reset prisma postgres install

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,13 @@
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const client = globalForPrisma.prisma || new PrismaClient();
+const client = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 export default client;
 
