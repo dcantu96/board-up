0ad6e6279df57f12da404022e9ff7ac581b9c12a
David Cantu <davidcantu966@gmail.com>
2025-07-04 16:37:10 -0600

    feat: implement NextAuth configuration with Google provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,19 +1,11 @@
-// lib/auth.config.ts (safe to import in middleware)
-import { NextAuthConfig } from "next-auth";
-import Google from "next-auth/providers/google";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
-export const authConfig: NextAuthConfig = {
-  secret: process.env.NEXTAUTH_SECRET,
-  adapter: PrismaAdapter(prisma), // ❗used only on server
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  session: {
-    strategy: "database",
-  },
-};
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
