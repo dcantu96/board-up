ce29731fa669103a80ff06b35504b23282cb53f3
David Cantu <davidcantu966@gmail.com>
2025-07-04 16:37:26 -0600

    feat: refactor middleware to use NextAuth for session management

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,19 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { isAuthenticated } from "@/auth-edge";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { NextResponse, type NextRequest } from "next/server";
+
+const { auth } = NextAuth(authConfig);
 
 export async function middleware(request: NextRequest) {
-  const authenticated = await isAuthenticated(request);
-
-  if (!authenticated) {
-    const url = new URL("/api/auth/signin", request.url);
-    url.searchParams.set("callbackUrl", request.url);
-    return NextResponse.redirect(url);
+  const session = await auth();
+  if (!session?.user) {
+    const signInUrl = new URL("/api/auth/signin", request.url);
+    signInUrl.searchParams.set(
+      "callbackUrl",
+      request.nextUrl.pathname + request.nextUrl.search
+    );
+    return NextResponse.redirect(signInUrl);
   }
-
   return NextResponse.next();
 }
 
