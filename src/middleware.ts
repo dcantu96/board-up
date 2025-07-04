947172b320c9c019b956a2bf34dd1b6e0bce6d79
David Cantu <davidcantu966@gmail.com>
2025-07-03 18:29:39 -0600

    feat: add middleware for authentication handling

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import { auth } from "@/auth"; // your custom auth config path
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const session = await auth();
+}
