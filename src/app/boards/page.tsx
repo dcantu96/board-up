040e0b91c79156d59e5228e139d59232dc2c44b8
David Cantu <davidcantu966@gmail.com>
2025-07-12 15:51:18 -0600

    sdfsdf

--- src/app/boards/page.tsx
+++ src/app/boards/page.tsx
@@ -6,6 +6,7 @@ export const dynamic = "force-dynamic";
 export default async function BoardsPage() {
   const boards = await prisma.board.findMany();
 
+  ("");
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] items-center sm:items-start">
